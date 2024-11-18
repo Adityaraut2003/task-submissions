5c8e4592d475c314f568e13d8b49dbbd92132656
Aditya Raut <165770232+Adityaraut2003@users.noreply.github.com>
2024-11-18 12:53:11 +0530

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from app.utils import add_numbers
+
+def main():
+    num1 = 10
+    num2 = 20
+    result = add_numbers(num1, num2)
+    print(f"Result of addition: {result}")
+
+if __name__ == "__main__":
+    main()
